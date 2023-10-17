c0d19bf9709dbff0326a6c918baa7f89dc1d4151
Daniil Cherepkov <groznybear@gmail.com>
2023-10-17 14:36:31 +0300

    feat: manual exits are added for certain cases

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -9,6 +13,22 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	handleSigTerms()
+
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":3000", nil)
+
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Println("http.ListenAndServe:", err)
+		os.Exit(1)
+	}
+}
+
+func handleSigTerms() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("received SIGTERM, exiting")
+		os.Exit(1)
+	}()
 }
